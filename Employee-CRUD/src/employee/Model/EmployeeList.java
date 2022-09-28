193f5bd5cbdde1556eea0d19f0100dff039710c0
barathis98 <114298089+barathis98@users.noreply.github.com>
2022-09-27 22:04:01 -0400

    Added EmployeeList

--- Employee-CRUD/src/employee/Model/EmployeeList.java
+++ Employee-CRUD/src/employee/Model/EmployeeList.java
@@ -4,10 +4,37 @@
  */
 package employee.Model;
 
+import java.util.ArrayList;
 /**
  *
  * @author BARATHI
  */
 public class EmployeeList {
-    
+    private ArrayList <Employee> list;
+    public EmployeeList()
+    {
+        this.list=new ArrayList<Employee>();       
+    }
+    public ArrayList<Employee> getEmployeeList()
+    {
+        return this.list;
+    }
+    public void setEmployeeList(ArrayList <Employee> list)
+    {
+        this.list=list;
+    }
+    public Employee addEmployee()
+    {
+        Employee emp=new Employee();
+        list.add(emp);
+        return emp;
+    }
+    public void deleteEmp(Employee emp)
+    {
+        list.remove(emp);
+    }
+    public void updateSelectedEmp(Employee emp)
+    {
+        list.add(emp);
+    }
 }

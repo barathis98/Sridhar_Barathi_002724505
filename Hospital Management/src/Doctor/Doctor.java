20d8defecdc6895401c414e1a9d1a8437a1b3814
barathis98 <114298089+barathis98@users.noreply.github.com>
2022-10-16 15:54:22 -0400

    Create new project Hospital Management for Assignment 2

---
+++ Hospital Management/src/Doctor/Doctor.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Doctor;
+
+/**
+ *
+ * @author BARATHI
+ */
+public class Doctor {
+    String name;
+    String username;
+    String password;
+    int phno;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhno(int phno) {
+        this.phno = phno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getPhno() {
+        return phno;
+    }
+}
---
+++ Hospital Management/src/hospital/management/HospitalManagement.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hospital.management;
+
+import java.awt.BorderLayout;
+
+/**
+ *
+ * @author BARATHI
+ */
+public class HospitalManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainScreen ms=new MainScreen();
+        ms.setVisible(true);
+        LoginPanel lp=new LoginPanel();
+        ms.add(lp,BorderLayout.CENTER);
+    }
+    
+}
